2f0cbc71c186883390d198b87c949447055bda0e
hyuk0309 <bahngeh@naver.com>
2023-02-22 07:42:49 +0900

    test : test sequence performance compare to normal collection API

--- src/main/kotlin/com/pteam/kotlin/hyuk/chapter08/code/MyFunction.kt
+++ src/main/kotlin/com/pteam/kotlin/hyuk/chapter08/code/MyFunction.kt
@@ -11,3 +11,13 @@ fun myNormalFunction(arg: Int = 100, operation: (Int) -> Unit) {
     operation(arg)
     println("End myNormalFunction")
 }
+
+fun getOddsUsingSequence(list: List<Int>): List<Int> {
+    return list.asSequence()
+        .filter { it % 2 == 1 }
+        .toList()
+}
+
+fun getOdds(list: List<Int>): List<Int> {
+    return list.filter { it % 2 == 1 }.toList()
+}
--- src/test/kotlin/com/pteam/kotlin/hyuk/chapter07/code/chapter08/code/MyFunctionTest.kt
+++ src/test/kotlin/com/pteam/kotlin/hyuk/chapter07/code/chapter08/code/MyFunctionTest.kt
@@ -1,5 +1,7 @@
 package com.pteam.kotlin.hyuk.chapter07.code.chapter08.code
 
+import com.pteam.kotlin.hyuk.chapter08.code.getOdds
+import com.pteam.kotlin.hyuk.chapter08.code.getOddsUsingSequence
 import com.pteam.kotlin.hyuk.chapter08.code.myInlineFunction
 import com.pteam.kotlin.hyuk.chapter08.code.myNormalFunction
 import io.kotest.core.spec.style.StringSpec
@@ -27,5 +29,19 @@ class MyFunctionTest : StringSpec({
         println("executionTimeOfInlineFunction : $executionTimeOfInlineFunction")
         println("executionTimeOfNormalFunction : $executionTimeOfNormalFunction")
         (executionTimeOfInlineFunction < executionTimeOfNormalFunction) shouldBe true
+    }
+
+    "performance test `using sequence vs not using sequence`" {
+        val executionTimeOfSequence = measureNanoTime {
+            getOddsUsingSequence((1..100).toList())
+        }
+
+        val executionTImeOfNormal = measureNanoTime {
+            getOdds((1..100).toList())
+        }
+
+        println("executionTimeOfSequence : $executionTimeOfSequence")
+        println("executionTImeOfNormal : $executionTImeOfNormal")
+        (executionTimeOfSequence > executionTImeOfNormal) shouldBe true
     }
 })
